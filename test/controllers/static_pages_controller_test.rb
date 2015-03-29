cb1b822919dceb74c380695c6fda4b239cd42313
James Youn <jamesyoun710@gmail.com>
2015-03-28 17:41:51 -0700

    Add contact page & supporting files

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
@@ -14,9 +15,14 @@ class StaticPagesControllerTest < ActionController::TestCase
   end
 
   test "should get about" do
-  	get :about
-  	assert_response :success
-  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
 end
