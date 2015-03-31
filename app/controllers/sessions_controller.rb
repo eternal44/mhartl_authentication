7660dd8a795dfa8b03aa192ec86ad59eac8b728a
James Youn <jamesyoun710@gmail.com>
2015-03-31 10:26:54 -0700

    add logout functions, links, and supporting files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,17 @@
 class SessionsController < ApplicationController
 
+  def current_user
+  	@current_user ||= User.find_by(id: session[:user_id])
+  end
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' 
       render 'new'
@@ -14,5 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+  	log_out
+  	redirect_to root_url
   end
 end
