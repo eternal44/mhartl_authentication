a1cbb2f88b8513d01a823f32b2162f4515b273c2
James Youn <jamesyoun710@gmail.com>
2015-04-02 02:12:41 -0700

    add minor edits to remember_digest migrate file - remove comments

--- db/migrate/20150402071827_add_remember_digest_to_users.rb
+++ db/migrate/20150402071827_add_remember_digest_to_users.rb
@@ -1,6 +1,5 @@
 class AddRememberDigestToUsers < ActiveRecord::Migration
-  #def change
-  	#this column already exists - I messed up with a previous merge... shouldn't be a problem if i comment it out.
-    #add_column :users, :remember_digest, :string
-  #end
+  def change
+    add_column :users, :remember_digest, :string
+  end
 end
